6a247c6687fe18a99756e81783a0b008b6192298
Soumaré Soulé <sourtoumo@gmail.com>
2023-10-07 19:50:58 +0000

    Correction in useEffect react

--- frontend/src/components/comments/CommentsList.jsx
+++ frontend/src/components/comments/CommentsList.jsx
@@ -4,15 +4,16 @@ import Comments from "./Comments";
 import Create from "./create";
 import ModalComments from "./ModalComments";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 
-export default function CommentsList({ setUser }) {
+export default function CommentsList() {
 
     const [allComments, setAllComments] = useState([]);
     const [show, setShow] = useState(false);
     const [parentId, setParentId] = useState("")
     const [comments, setComments] = useState([]);
-    const [likeType, setLieType] = useState([])
+    const [likeType, setLikeType] = useState([])
+
     async function getAllComments() {
         await axios.post(`/api/comment`,
             {
@@ -28,39 +29,39 @@ export default function CommentsList({ setUser }) {
         });
     }
 
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
 
     useEffect(() => {
-
-        const getComments = async () => {
-            await axios.post(`/api/comment`, {
-                withCredentials: true,
-            }).then((res) => {
-                setAllComments(res.data)
-                return;
-            }).catch((e) => {
-                const message = `Une erreur se produit: ${e.message}`;
-                console.log(message)
-                return;
-            });
-        }
-        const getLikeType = async () => {
-            await axios.post(`/api/like/type/all`, {
-                withCredentials: true,
-            }).then((res) => {
-                setLieType(res.data)
-                return;
-            }).catch((e) => {
+        async function fetchData() {
+            try {
+                const response = await axios.post('/api/comment', {
+                    withCredentials: true,
+                });
+                setAllComments(response.data);
+            } catch (e) {
                 const message = `Une erreur se produit: ${e.message}`;
                 console.log(message);
-                return;
-            });
+            }
         }
-        getComments();
-        getLikeType()
 
-        return;
-    }, [allComments, comments, navigate, setUser])
+        async function fetchLikeType() {
+            try {
+                const response = await axios.post('/api/like/type/all', {
+                    withCredentials: true,
+                });
+                setLikeType(response.data);
+                console.log(response.data);
+            } catch (e) {
+
+                const message = `Une erreur se produit: ${e.message}`;
+                console.log(message);
+            }
+        }
+
+        fetchData();
+        fetchLikeType();
+    }, []);
+
     return (
         <div className=" pt-6 bg-white md:bg-gray-100 flex justify-center items-center min-h-screen selection:bg-red-500 selection:text-white">
             <div className='overflow-y-auto custome-scroll-bar pb-12 min-h-[98dvh] max-h-[98dvh] relative w-full md:w-[37rem] lg:w-[43rem] pt-6 bg-white'>
@@ -70,7 +71,7 @@ export default function CommentsList({ setUser }) {
                     !show &&
                     <>
                         <Comments likeType={likeType} setParentId={setParentId} setComments={setComments} setShow={setShow} comments={allComments} />
-                        <Create onSucces={getAllComments} />
+                        <Create onSucces={() => getAllComments} />
                     </>
                 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -17,6 +17,35 @@ import ProfileScreen from './screens/ProfileScreen.jsx';
 import PrivateRoute from './components/PrivateRoute.jsx';
 import { ThemeProvider } from '@material-tailwind/react';
 import CommentsList from './components/comments/CommentsList.jsx';
+import { registerSW } from 'virtual:pwa-register'
+
+const intervalMS = 60 * 60 * 1000
+
+const updateSW = registerSW({
+  onOfflineReady() { },
+  onRegisteredSW(swUrl, r) {
+    r && setInterval(async () => {
+      if (!(!r.installing && navigator))
+        return
+
+      if (('connection' in navigator) && !navigator.onLine)
+        return
+
+      const resp = await fetch(swUrl, {
+        cache: 'no-store',
+        headers: {
+          'cache': 'no-store',
+          'cache-control': 'no-cache',
+        },
+      })
+
+      if (resp?.status === 200)
+        await r.update()
+    }, intervalMS)
+  }
+})
+
+updateSW()
 
 const router = createBrowserRouter(
   createRoutesFromElements(
