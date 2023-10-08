18b1376e3621bb8b75afd5c21a449289b9dd6eb9
Soumaré Soulé <sourtoumo@gmail.com>
2023-10-08 12:46:28 +0000

    Correction in useEffect react

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = import.meta.env.VITE_NODE_ENV || undefined;
+const URL = "https://sds-social.vercel.app/";
 
 export const socket = io(URL, {
   withCredentials: true,
