772a74d28bc83b8efdf931841e310a160ba3f6d3
Soumaré Soulé <sourtoumo@gmail.com>
2023-10-08 12:33:49 +0000

    Correction in useEffect react

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,5 +1,12 @@
 import { io } from "socket.io-client";
 
-export const socket = io("https://sds-social.vercel.app/", {
+// "undefined" means the URL will be computed from the `window.location` object
+const URL =
+  import.meta.env.VITE_NODE_ENV === "production"
+    ? undefined
+    : "http://localhost:5000/";
+
+export const socket = io(URL, {
   withCredentials: true,
+  transports: ["websocket"], // Required when using Vite
 });
