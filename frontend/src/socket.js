d3f5672cee832612ea7109246f97aebf8c25b9fc
Soumaré Soulé <sourtoumo@gmail.com>
2023-10-08 15:34:31 +0000

    Correction in useEffect react

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -2,9 +2,9 @@ import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
 // const URL = import.meta.env.VITE_NODE_ENV || undefined;
-const URL = undefined;
+// const URL = undefined;
 
-export const socket = io(URL, {
+export const socket = io(undefined, {
   withCredentials: true,
   transports: ["websocket"], // Required when using Vite
   path: "/api/socket",
